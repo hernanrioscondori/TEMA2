791ac060e38b57ddf5e74a073f1e4db594d8c9e0
hernanrioscondori <103694996+hernanrioscondori@users.noreply.github.com>
2022-11-08 07:34:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import {GLTFLoader} from "GLTFLoader.js";
-
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
   //  mockWithVideo('tema2.mp4');
-
     const mindarThree = new window.MINDAR.IMAGE.mindarThree({
       container: document.body,
       imageTargetSrc: "targets.mind",
     });
     const {renderer, scene, camera} = mindarThree;
+    const light = new THREE.HemisphereLight(0Xffffff, 0Xbbbbbff, 1);
+    scene.add(light);
  const anchor = mindarThree.addAnchor(0);
     const loader= new GLTFLoader();
     loader.load("scene.gltf", (gltf) =>{
+      gltf.scene.scale.set(0.1, 0.1, 0.1);
+      gltf.scene.position.set(0, -0.4, 0);
      anchor.group.add(gltf.scene);
     });
 
