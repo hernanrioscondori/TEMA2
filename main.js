cb344ab1e9912b339b57b59dd69e7d19beef6101
hernanrioscondori <103694996+hernanrioscondori@users.noreply.github.com>
2022-11-08 00:23:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import {loadGLTF} from "loader.js";
-import {mockWithVideo} from 'camera-mock';
+import {mockWithVideo} from 'camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
-    mockWithVideo('tema2.mp4');
+  //  mockWithVideo('tema2.mp4');
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
